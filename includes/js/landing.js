5d11e99786564a7e2e10caa0fe67071525483f32
Matt Weeks <m_weeks@hotmail.co.uk>
2014-10-29 13:24:12 +0000

    Add login links. Change indentation to 4 spaces. #kentprojects-38 #kentprojects-40

--- includes/js/landing.js
+++ includes/js/landing.js
@@ -4,11 +4,11 @@
  *@param {Number} num The integer value signifying the percentage opacity of the element.
  */
 function fadeIn(elem, num) {
-  document.getElementById(elem).style.filter = "alpha(opacity=" + num + ")";
-  document.getElementById(elem).style.opacity = num/50;
-  if (num < 50) {
-    window.setTimeout(function(){fadeIn(elem, num+1)},1);
-  }
+    document.getElementById(elem).style.filter = "alpha(opacity=" + num + ")";
+    document.getElementById(elem).style.opacity = num/50;
+    if (num < 50) {
+        window.setTimeout(function(){fadeIn(elem, num+1)},1);
+    }
 }
 
 /**
@@ -17,39 +17,39 @@ function fadeIn(elem, num) {
  *@param {Number} num The integer value signifying the percentage opacity of the element.
  */
 function fadeOut(elem, num) {
-  document.getElementById(elem).style.filter = "alpha(opacity=" + num + ")";
-  document.getElementById(elem).style.opacity = num/50;
-  if (num > 0) {
-    window.setTimeout(function(){fadeOut(elem, num-1)},1);
-  }
+    document.getElementById(elem).style.filter = "alpha(opacity=" + num + ")";
+    document.getElementById(elem).style.opacity = num/50;
+    if (num > 0) {
+        window.setTimeout(function(){fadeOut(elem, num-1)},1);
+    }
 }
 
 /**
  * Changes the info button text and style. Begins the fade sequence to hide the info div.
  */
 function hideInfo() {
-  document.getElementById("btnInfo").innerHTML = "?";
-  fadeOut("infoContainer", 50);
-  document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-danger\b/, "btn-info");
+    document.getElementById("btnInfo").innerHTML = "?";
+    fadeOut("infoContainer", 50);
+    document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-danger\b/, "btn-info");
 }
 
 /**
  * Changes the info button text and style. Begins the fade sequence to show the info div.
  */
 function showInfo() {
-  document.getElementById("btnInfo").innerHTML = "X";
-  fadeIn("infoContainer", 0);
-  document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-info\b/, "btn-danger");
+    document.getElementById("btnInfo").innerHTML = "X";
+    fadeIn("infoContainer", 0);
+    document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-info\b/, "btn-danger");
 }
 
 /**
  * Switches the opacity of the information div from 0 to 1 or from 1 to 0.
  */
 function switchInfoVisibility() {
-  if (document.getElementById("infoContainer").style.opacity == 0) {
-    showInfo();
-  }
-  else {
-    hideInfo();
-  }
+    if (document.getElementById("infoContainer").style.opacity == 0) {
+        showInfo();
+    }
+    else {
+        hideInfo();
+    }
 }
