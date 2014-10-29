8104140d7d0efd6bb185d5f7872c4a0c69e0602a
Matt Weeks <m_weeks@hotmail.co.uk>
2014-10-29 01:43:34 +0000

    Add JS to show/hide information div. #kentprojects-40

--- includes/js/landing.js
+++ includes/js/landing.js
@@ -12,11 +12,13 @@ function switchInfoVisibility() {
 function hideInfo() {
 	infoHidden = true;
 	document.getElementById("btnInfo").innerHTML = "?";
+	document.getElementById("divInfo").style.display = "none";
 	document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-danger\b/, "btn-info");
 }
 
 function showInfo() {
 	infoHidden = false;
 	document.getElementById("btnInfo").innerHTML = "X";
+	document.getElementById("divInfo").style.display = "block";
 	document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-info\b/, "btn-danger");
 }
