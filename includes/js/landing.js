b8638120b2b4c4296695b02c0897ae2d911fbdd9
Matt Weeks <m_weeks@hotmail.co.uk>
2014-10-29 03:41:09 +0000

    Add fade functionality for info div. #kentprojects-40

--- includes/js/landing.js
+++ includes/js/landing.js
@@ -12,13 +12,27 @@ function switchInfoVisibility() {
 function hideInfo() {
 	infoHidden = true;
 	document.getElementById("btnInfo").innerHTML = "?";
-	document.getElementById("divInfo").style.display = "none";
+	fadeOut("btnInfo", 100);
 	document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-danger\b/, "btn-info");
+}
+
+function fadeIn(elem, num) {
+	document.getElementById("infoContainer").style.opacity = num/100;
+	if (num < 100) {
+		window.setTimeout(function(){fadeIn(elem, num+1)},4);
+	}
+}
+
+function fadeOut(elem, num) {
+	document.getElementById("infoContainer").style.opacity = num/100;
+	if (num > 0) {
+		window.setTimeout(function(){fadeOut(elem, num-1)},4);
+	}
 }
 
 function showInfo() {
 	infoHidden = false;
 	document.getElementById("btnInfo").innerHTML = "X";
-	document.getElementById("divInfo").style.display = "block";
+	fadeIn("btnInfo", 0);
 	document.getElementById("btnInfo").className = document.getElementById("btnInfo").className.replace(/\bbtn-info\b/, "btn-danger");
 }
