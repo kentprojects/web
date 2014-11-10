14548a9e2feff2ffe2c8046ee2740a47d8f7e8b3
jdrydn <james@jdrydn.com>
2014-11-10 13:28:43 +0000

    CSS updates (just relative file fixes) and moved the login page to a PHP file to change the API URL when needed.

--- public/login.php
+++ public/login.php
@@ -1,3 +1,10 @@
+<?php
+$api_url = "http://api.dev.kentprojects.com";
+if (!empty($_SERVER["VAGRANT_ENV"]) && ($_SERVER["VAGRANT_ENV"] === "true"))
+{
+	$api_url = "http://localhost:8060";
+}
+?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -6,7 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> Kent Projects Login </title>
         <link rel="shortcut icon" href="/includes/img/kp.ico">
-        <link rel="stylesheet" href="/includes/css/bootstrap.min.css">
+        <link href="/includes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="/includes/css/flat-ui-pro.min.css" rel="stylesheet">
         <link href="/includes/css/style.css" rel="stylesheet">
     </head>
@@ -17,22 +24,22 @@
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=f4dfeada0e91e1791a80da1bb26a7d96" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Convenor </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=f4dfeada0e91e1791a80da1bb26a7d96" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Convenor </a>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=1e9a755d73865da9068f079d81402ce7" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Supervisor 1 </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=1e9a755d73865da9068f079d81402ce7" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Supervisor 1 </a>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=6f2653c2a1c64220e3d2a713cc52b438" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Supervisor 2 </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=6f2653c2a1c64220e3d2a713cc52b438" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Supervisor 2 </a>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=1f18ed87771daf095e090916cb9423e4" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 1 </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=1f18ed87771daf095e090916cb9423e4" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 1 </a>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=1460357d62390ab9b3b33fa1a0618a8f" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 2 </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=1460357d62390ab9b3b33fa1a0618a8f" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 2 </a>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 big-margin">
-                    <a href="http://api.dev.kentprojects.com/auth/internal?auth=930144ea545ce754789b15074106bc36" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 3 </a>
+                    <a href="<?php echo $api_url;?>/auth/internal?auth=930144ea545ce754789b15074106bc36" class="btn btn-block btn-md btn-primary center-item restricted-width"> Log in as Student 3 </a>
                 </div>
             </div>
             <div class="row">
