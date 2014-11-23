e0fe894b5002b7c2ba45e72a2fc2114645928627
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-23 18:33:54 +0000

    Initial setup of HTML and CSS. #kentprojects-112

---
+++ public/includes/php/header.php
@@ -0,0 +1,13 @@
+<header class="kentBlueBackground">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-8 col-sm-8 col-md-8 no-right-padding">
+				<h4 class="inline-heading whiteText"> Kent Projects </h4>
+			</div>
+			<div class="col-xs-4 col-sm-4 col-md-4 login-position no-left-padding alignRight">
+				<span class="fui-gear iconSize whiteText"></span>
+				<span class="fui-exit iconSize whiteText"></span>
+			</div>
+		</div>
+	</div>
+</header>
