aabd69bc66dd2227322c9b4624c7c452eeda49ab
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-24 15:06:09 +0000

    Link the Kent Projects text to the user dashbard. #kentprojects-114

--- public/includes/php/header.php
+++ public/includes/php/header.php
@@ -2,7 +2,8 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-8 col-sm-8 col-md-8 no-right-padding">
-				<h4 class="inline-heading whiteText"> Kent Projects </h4>
+				<!-- TODO: Change link to actual dashboard link -->
+				<h4 class="inline-heading whiteText"><a href="#dashboard"> Kent Projects </a></h4>
 			</div>
 			<div class="col-xs-4 col-sm-4 col-md-4 login-position no-left-padding alignRight">
 				<span class="fui-gear iconSize whiteText" onclick="clickSettings()"></span>
