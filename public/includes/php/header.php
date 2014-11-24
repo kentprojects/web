e037e6a59460a1303bd00f94b146e027d5e8347a
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-24 15:54:15 +0000

    Restyle links. #kentprojects-114

--- public/includes/php/header.php
+++ public/includes/php/header.php
@@ -2,18 +2,21 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-8 col-sm-8 col-md-8 no-right-padding">
-				<!-- TODO: Change link to actual dashboard link -->
+				<!-- TODO: Change link to actual dashboard link. -->
 				<a href="#dashboard"><h4 class="inline-heading whiteText"> Kent Projects </h4></a>
 			</div>
 			<div class="col-xs-4 col-sm-4 col-md-4 login-position no-left-padding alignRight">
 				<span class="fui-gear iconSize whiteText" onclick="clickSettings()"></span>
 				<span class="fui-exit iconSize whiteText" onclick="clickLogOut()"></span>
 			</div>
+		</div>
+		<div class="row">
 			<div class="hideOnLoad" id="settingsMenu">
 				<ul class="whiteText" id="settingsList">
-					<li class="settingsMenuOption"><a class="whiteText" href="#option1">Option 1</a></li>
-					<li class="settingsMenuOption"><a class="whiteText" href="#option2">Option 2</a></li>
-					<li class="settingsMenuOption whiteText"><a class="whiteText" href="#option3">Option 3</a></li>
+					<!-- TODO: Replace with proper links and options text. -->
+					<li class="settingsMenuOption"><a class="listLink whiteText" href="#option1">Option 1</a></li>
+					<li class="settingsMenuOption"><a class="listLink whiteText" href="#option2">Option 2</a></li>
+					<li class="settingsMenuOption"><a class="listLink whiteText" href="#option3">Option 3</a></li>
 				</ul>
 			</div>
 		</div>
