88a7096782808f2b580fc54aa785ddae269dcc1b
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-25 14:40:23 +0000

    Collect idiocy.

--- public/includes/php/footer.php
+++ public/includes/php/footer.php
@@ -1,5 +1,5 @@
-<footer class="row">
-    <div class="container">
+<footer class="container">
+    <div class="row">
         <hr/>
         <p class="big-margin soften-text text-center"> &copy; 2014. James Dryden, Matt House, Matt Weeks </p>
     </div>
