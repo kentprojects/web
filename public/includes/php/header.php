eb03ff93d1c35ce81387bf6592ceef3061597586
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-27 14:24:52 +0000

    Fix icon view on mobile portrait view. #kentprojects-114

--- public/includes/php/header.php
+++ public/includes/php/header.php
@@ -1,10 +1,10 @@
 <header class="kentBlueBackground">
     <div class="container">
         <div class="row">
-            <div class="col-xs-8 col-sm-8 col-md-8 no-right-padding">
+            <div class="col-xs-7 col-sm-7 col-md-7 no-right-padding">
                 <a href="dashboard.php"><h4 class="inline-heading smallerMobileHeading whiteText"> Kent Projects </h4></a>
             </div>
-            <div class="col-xs-4 col-sm-4 col-md-4 login-position no-left-padding alignRight">
+            <div class="col-xs-5 col-sm-5 col-md-5 login-position no-left-padding alignRight">
                 <a href="profile.php"><span class="fui-user smallerMobileHeading whiteText"></span></a>
                 <a href="settings.php"><span class="fui-gear marginLeft10 smallerMobileHeading whiteText"></span></a>
                 <span class="fui-exit hoverHand marginLeft10 smallerMobileHeading whiteText" onclick="logoutUser()"></span>
