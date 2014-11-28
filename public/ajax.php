d2299aaf12548793d20048b63e6dd7aeafe695bb
jdrydn <james@jdrydn.com>
2014-11-28 17:38:05 +0000

    Updated the AJAX script, but it needs finishing.

--- public/ajax.php
+++ public/ajax.php
@@ -1,2 +1,28 @@
 <?php
-require_once __DIR__."/../ajax.php";
+/**
+ * @author: KentProjects <developer@kentprojects.com>
+ * @license: Copyright KentProjects
+ * @link: http://kentprojects.com
+ *
+ * Here one should ensure we block any requests from an external source.
+ */
+require_once __DIR__ . "/../private/api.php";
+try
+{
+	$postData = json_decode(file_get_contents("php://input"), true);
+	if (empty($postData["method"]) || empty($postData["url"]))
+	{
+		throw new InvalidArgumentException("Missing method and/or URL.");
+	}
+
+	$response = API::Request(
+		"api:request:" . strtolower($postData["method"]),
+		$postData["url"],
+		$postData["query"],
+		$postData["post"]
+	);
+}
+catch (Exception $e)
+{
+	header("HTTP/1.1 500 Internal Server Error");
+}
