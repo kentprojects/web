190bd07a5fb410a8b33b8555416fd1fbc085cfa0
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-28 13:33:34 +0000

    Create templates directory.

--- public/templates/template.php
+++ public/templates/template.php
@@ -5,24 +5,24 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title> Template </title>
-        <link rel="shortcut icon" href="/includes/img/kp.ico">
-        <link href="/includes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="/includes/css/flat-ui-pro.min.css" rel="stylesheet">
-        <link href="/includes/css/style.css" rel="stylesheet">
-        <script src="/includes/js/konami.js">
+        <link rel="shortcut icon" href="/../includes/img/kp.ico">
+        <link href="../includes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="../includes/css/flat-ui-pro.min.css" rel="stylesheet">
+        <link href="../includes/css/style.css" rel="stylesheet">
+        <script src="../includes/js/konami.js">
             var easter_egg = new Konami(function() { alert('Konami code!')});
             easter_egg.load();
         </script>
     </head>
     <body>
 
-        <?php include 'includes/php/header.php'; ?>
+        <?php include '../includes/php/header.php'; ?>
         <div class="container">
             <div class="row">
                 <h1> Page content </h1>
                 <p> This be your page content... </p>
             </div>
         </div>
-        <?php include 'includes/php/footer.php'; ?>
+        <?php include '../includes/php/footer.php'; ?>
     </body>
 </html>
