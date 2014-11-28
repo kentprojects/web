97509b4ba75a06b85e794cbb79a175e3377bd308
h0use <matt@matthewhou.se>
2014-11-28 12:00:14 +0000

    Has anyone ever gone so far as to want more look like?

--- public/profile.php
+++ public/profile.php
@@ -1,6 +1,8 @@
 <?php
 $prerequisites = array("authentication");
 require_once __DIR__."/../private/bootstrap.php";
+
+$user = Auth::getUser();
 ?>
 <!doctype html>
 <html lang="en">
@@ -19,7 +21,7 @@ require_once __DIR__."/../private/bootstrap.php";
         <div class="container">
             <div class="row">
                 <h1> Profile </h1>
-                <p> This be your profile page, <?php echo Session::get("user");?> </p>
+                <p> This be your profile page, <?php echo $user->first_name;?> </p>
             </div>
         </div>
         <?php include 'includes/php/footer.php'; ?>
