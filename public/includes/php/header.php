a64ef1605711da56c23e6dfe6d10e6e6e0b7cd14
Matt Weeks <m_weeks@hotmail.co.uk>
2014-11-28 13:44:19 +0000

    Fix links.

--- public/includes/php/header.php
+++ public/includes/php/header.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-7 col-sm-7 col-md-7 no-right-padding">
-                <a href="dashboard.php"><h4 class="inline-heading smallerMobileHeading whiteText"> Kent Projects </h4></a>
+                <a href="/dashboard.php"><h4 class="inline-heading smallerMobileHeading whiteText"> Kent Projects </h4></a>
             </div>
             <div class="col-xs-5 col-sm-5 col-md-5 login-position no-left-padding alignRight">
-                <a href="profile.php"><span class="fui-user smallerMobileHeading whiteText"></span></a>
-                <a href="settings.php"><span class="fui-gear marginLeft10 smallerMobileHeading whiteText"></span></a>
+                <a href="/profile.php"><span class="fui-user smallerMobileHeading whiteText"></span></a>
+                <a href="/settings.php"><span class="fui-gear marginLeft10 smallerMobileHeading whiteText"></span></a>
                 <span class="fui-exit hoverHand marginLeft10 smallerMobileHeading whiteText" onclick="logoutUser()"></span>
             </div>
         </div>
