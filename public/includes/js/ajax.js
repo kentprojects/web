2bdbf1ea5454a6f656a5d796a49f393b91f6087c
jdrydn <james@jdrydn.com>
2014-12-11 20:05:31 +0000

    Updated to the Ajax JS. Requires additional testing. #kentprojects-169 work development 10m

--- public/includes/js/ajax.js
+++ public/includes/js/ajax.js
@@ -1,28 +1,118 @@
 var API = {};
 
-API.GET = function(url, query, success, error) {
+/**
+ * Perform a GET request.
+ *
+ * API.GET("/user/1", {}, function(response) {
+ *     var user = response.body;
+ *     console.log(user.email);
+ * });
+ *
+ * @param url The endpoint to request.
+ * @param query Any query data (as an object).
+ * @param success The function to run upon success
+ * @param error The function to run upon error.
+ * @return void
+ */
+API.GET = function (url, query, success, error)
+{
 	API._Request("GET", url, query, {}, success, error)
 };
 
-API.POST = function( url, data, success, error) {
-	API._Request("POST",url, {}, data, success, error)
+/**
+ * Perform a POST request.
+ *
+ * API.POST(
+ *     "/user",
+ *     {
+ *         "email": "matt.house@kentprojects.com"
+ *     },
+ *     function(response)
+ *     {
+ *         console.log(response.status); // 201
+ *         console.log(response.body.email); // matt.house@kentprojects.com
+ *     }
+ * );
+ *
+ * @param url The endpoint to request.
+ * @param post Any post data (as an object).
+ * @param success The function to run upon success
+ * @param error The function to run upon error.
+ * @return void
+ */
+API.POST = function (url, post, success, error)
+{
+	API._Request("POST", url, {}, post, success, error)
 };
 
-API.PUT = function(url, data, success, error) {
-	API._Request("PUT", url, {}, data, success, error)
+/**
+ * Perform a PUT request.
+ *
+ * API.PUT(
+ *     "/user/1",
+ *     {
+ *         "email": "matt.house@kentprojects.com"
+ *     },
+ *     function(response)
+ *     {
+ *         console.log(response.status); // 200
+ *         console.log(response.body.email); // matt.house@kentprojects.com
+ *     }
+ * );
+ *
+ * @param url The endpoint to request.
+ * @param post Any post data (as an object).
+ * @param success The function to run upon success
+ * @param error The function to run upon error.
+ * @return void
+ */
+API.PUT = function (url, post, success, error)
+{
+	API._Request("PUT", url, {}, post, success, error)
 };
 
-API.DELETE = function(url, query, success, error) {
-	API._Request("DELETE", url, query, success, error)
+/**
+ * Perform a DELETE request.
+ *
+ * API.DELETE("/user/1", {}, function(response) {
+ *     console.log(response.status); // 200
+ *     console.log(response.body.email); // UNDEFINED
+ * });
+ *
+ * @param url The endpoint to request.
+ * @param query Any query data (as an object).
+ * @param success The function to run upon success
+ * @param error The function to run upon error.
+ * @return void
+ */
+API.DELETE = function (url, query, success, error)
+{
+	API._Request("DELETE", url, query, {}, success, error)
 };
 
-API._Request = function(method, url, query, post, success, error) {
-	var data = data || {},
-		r = new XMLHttpRequest();
-	r.open("POST", "/ajax.php"+url, true);
-	r.onreadystatechange = function () {
-		if (r.readyState != 4) return;
-		// console.log(r, r.responseText);
+/**
+ * Performs the main request.
+ * This should not be run by hand.
+ *
+ * @param method The method to request.
+ * @param url The endpoint to request.
+ * @param query Any query data (as an object).
+ * @param post Any post data (as an object).
+ * @param success The function to run upon success
+ * @param error The function to run upon error.
+ * @return void
+ */
+API._Request = function (method, url, query, post, success, error)
+{
+	var r = new XMLHttpRequest();
+	r.open("POST", "/ajax.php", true);
+	r.onreadystatechange = function ()
+	{
+		if (r.readyState != 4)
+		{
+			return;
+		}
+		// console.log(r.responseText);
 		if ((r.status >= 200) && (r.status < 300))
 		{
 			success && success(JSON.parse(r.responseText));
@@ -32,10 +122,10 @@ API._Request = function(method, url, query, post, success, error) {
 			error && error(JSON.parse(r.responseText))
 		}
 	};
-	r.send(JSON.stringify(data));
+	r.send(JSON.stringify({
+		"method": method || null,
+		"url": url || null,
+		"query": query || [],
+		"post": post || []
+	}));
 };
-
-// example usage:
-// API.GET("/user/1", {}, function(user) {
-//	  console.log(user.email);
-// });
