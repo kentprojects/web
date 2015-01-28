da7ba6a9069b76036b802097151b153d2debb6af
h0use <matt@matthewhou.se>
2015-01-28 14:52:23 +0000

    More messing

--- public/login.php
+++ public/login.php
@@ -107,17 +107,21 @@ $people = array(
 				</div>
 				<?php if ($loginAsCas) { ?>
 					<div class="col-xs-12 col-sm-6 col-md-4 bigMargin centerInRow centerItem">
-						<div class="login-form">
-				        	<div class="form-group">
-				            	<input type="text" class="form-control login-field" value="" placeholder="Enter your name" id="login-name" />
-								<label class="login-field-icon fui-user" for="login-name"></label>
-				            </div>
+						<div class="login-screen">
+				          <div class="login-form">
 				            <div class="form-group">
-				            	<input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
-								<label class="login-field-icon fui-lock" for="login-pass"></label>
+				              <input type="text" class="form-control login-field" value="" placeholder="Enter your name" id="login-name" />
+				              <label class="login-field-icon fui-user" for="login-name"></label>
 				            </div>
+				
+				            <div class="form-group">
+				              <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
+				              <label class="login-field-icon fui-lock" for="login-pass"></label>
+				            </div>
+				
 				            <a class="btn btn-primary btn-lg btn-block" href="#">Log in</a>
 				            <a class="login-link" href="#">Lost your password?</a>
+				          </div>
 				        </div>
 					</div>
 				<?php } ?>
