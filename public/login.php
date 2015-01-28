1516416d6643d2e69cdc2005bcc04008903b9f0d
h0use <matt@matthewhou.se>
2015-01-28 14:30:09 +0000

    More testing before I get my Vagrant box working

--- public/login.php
+++ public/login.php
@@ -36,9 +36,11 @@ if (!empty($_GET["auth"]))
 	exit();
 }
 
+$loginAsCas = false;
+
 if (isset($_GET["cas"]))
 {
-	die("Declan");
+	$loginAsCas = true;
 }
 
 /**
@@ -103,6 +105,26 @@ $people = array(
 						Login with Single-Sign-On
 					</a>
 				</div>
+				<?php 
+				if(loginAsCas { 
+					echo('
+					<div class="col-xs-12 col-sm-6 col-md-4 bigMargin centerInRow centerItem">
+						<div class="login-form">
+				        	<div class="form-group">
+				            	<input type="text" class="form-control login-field" value="" placeholder="Enter your name" id="login-name" />
+								<label class="login-field-icon fui-user" for="login-name"></label>
+				            </div>
+				            <div class="form-group">
+				            	<input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
+								<label class="login-field-icon fui-lock" for="login-pass"></label>
+				            </div>
+				            <a class="btn btn-primary btn-lg btn-block" href="#">Log in</a>
+				            <a class="login-link" href="#">Lost your password?</a>
+				        </div>
+					</div>
+					')
+				})
+			?>
 			</div>
             <div class="row">
 				<?php foreach($people as $code => $person) { ?>
