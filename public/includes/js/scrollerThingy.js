7d37d645b20b567900c1e5335dc66f45966a591a
Matt Weeks <m_weeks@hotmail.co.uk>
2015-03-02 09:52:21 +0000

    Comment out unused function.

--- public/includes/js/scrollerThingy.js
+++ public/includes/js/scrollerThingy.js
@@ -61,7 +61,7 @@ function scrollerTile(item, type, addStyle) {
 
 	// Create tile from HTML segments and return as a string.
 	tileHTML.push(
-		'<li id="' + type + item.id + '"' + classList + ' onclick="openLink(\'' + type + item.id + '\');">',
+		'<li>',// id="' + type + item.id + '"' + classList + ' onclick="openLink(\'' + type + item.id + '\');">',
 		// '<a id="' + type + item.id + '" class="tileLink" href="javascript:openLink(\'' + type + item.id + '\');"></a>',
 		'<div class="tile-title"><a href="/profile.php?type=' + type + '&id='+ item.id + '">' + item.name + '</a></div>',
 		'</div>',
@@ -73,34 +73,34 @@ function scrollerTile(item, type, addStyle) {
 	return tileHTML.join("");
 }
 
-function openLink(x) {
-	// console.log(document.getElementById(x));
-	// console.log(document.getElementById(x).parentNode);
+// function openLink(x) {
+// 	console.log(document.getElementById(x));
+// 	console.log(document.getElementById(x).parentNode);
 
-	var myNode = document.getElementById(x);
-	// console.log('x1: ' + myNode.offsetLeft);
-	// console.log('y1: ' + myNode.offsetTop);
-	// console.log('x2: ' + (myNode.offsetLeft + myNode.offsetWidth));
-	// console.log('y2: ' + (myNode.offsetTop + myNode.offsetHeight));
+// 	var myNode = document.getElementById(x);
+// 	console.log('x1: ' + myNode.offsetLeft);
+// 	console.log('y1: ' + myNode.offsetTop);
+// 	console.log('x2: ' + (myNode.offsetLeft + myNode.offsetWidth));
+// 	console.log('y2: ' + (myNode.offsetTop + myNode.offsetHeight));
 
-	var parentNode = document.getElementById(x).parentNode;
-	// console.log('x1: ' + parentNode.offsetLeft);
-	// console.log('y1: ' + parentNode.offsetTop);
-	// console.log('x2: ' + (parentNode.offsetWidth));
-	// console.log('y2: ' + (parentNode.offsetHeight));
+// 	var parentNode = document.getElementById(x).parentNode;
+// 	console.log('x1: ' + parentNode.offsetLeft);
+// 	console.log('y1: ' + parentNode.offsetTop);
+// 	console.log('x2: ' + (parentNode.offsetWidth));
+// 	console.log('y2: ' + (parentNode.offsetHeight));
 
-	// console.log(window.getComputedStyle(parentNode).getPropertyValue('transform'));
+// 	console.log(window.getComputedStyle(parentNode).getPropertyValue('transform'));
 
-	var matrix = window.getComputedStyle(parentNode).getPropertyValue('transform');
+// 	var matrix = window.getComputedStyle(parentNode).getPropertyValue('transform');
 
-    if(matrix !== 'none') {
-        var values = matrix.toString();
-        var translationValue = parseInt(values.split(",")[4].substr(1));
-        //console.log(translationValue);
+//     if(matrix !== 'none') {
+//         var values = matrix.toString();
+//         var translationValue = parseInt(values.split(",")[4].substr(1));
+//         console.log(translationValue);
 
-        // Discus thin page / mobile view handling.
-    }
-}
+//         Discus thin page / mobile view handling.
+//     }
+// }
 
 function scrollerHTML(data, type, addStyle) {
 	if (data.length > 0) {
