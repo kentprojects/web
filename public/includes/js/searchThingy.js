05c1d9b2b7b2213c48b59162cb192acc475cd5c5
Matt Weeks <m_weeks@hotmail.co.uk>
2015-03-18 16:58:01 +0000

    Fix scroller dots.

--- public/includes/js/searchThingy.js
+++ public/includes/js/searchThingy.js
@@ -6,7 +6,10 @@
 
 
 function stuff() {
+	var $frame = $('#projectScroller');
 	removeEDR(document.getElementById('navbarInput-01').value, "tileLiproject");
+	$frame.sly('reload');
+	console.log("slyd")
 }
 
 function removeEDR(stringFind, tileClass) {
@@ -15,7 +18,6 @@ function removeEDR(stringFind, tileClass) {
 		for (i = 0; i < tiles.length; i++) {
 			var tile = tiles[i];
 			tile.className = tile.className.replace(" hideTile", "");
-			// console.log(tile.firstChild.innerText)
 			if (tile.firstChild.innerText.toUpperCase().indexOf(stringFind.toUpperCase()) == -1) {
 				tile.className = tile.className + " hideTile";
 			}
