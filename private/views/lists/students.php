eb9d97673fa550b0364c094d3001e86c95dc4889
Matt Weeks <m_weeks@hotmail.co.uk>
2015-03-29 16:41:41 +0100

    Fix top of list view styling.

--- private/views/lists/students.php
+++ private/views/lists/students.php
@@ -1,18 +1,15 @@
 <div class="row">
-	<div class="col-xs-8 col-sm-4 col-md-4">
-		<h1 class="reduceHeading hideEdit">Students</h1>
+	<div class="col-xs-12 col-sm-8 col-md-8">
+		<h1 class="reduceHeading hideEdit float-left listHeading">Students</h1>
+
+		<div class="reduceTopMargin alignRight listButtonsDiv">
+			<div class="floatRight fui-new listButtons" onclick="alert();"></div>
+			<div class="floatRight fui-eye listButtons marginRight"onclick="changeListView();"></div>
+		</div>
 	</div>
-	<div class="reduceTopMargin col-xs-4 col-sm-2 col-md-2 alignRight">
-		<div class="floatRight fui-new listButtons" onclick="alert();"></div>
-		<div class="floatRight fui-eye listButtons marginRight"onclick="changeListView();"></div>
-	</div>
-
-
-
-
 
 	<!-- Search bit -->
-	<div class="col-xs-12 col-sm-6 col-md-6">
+	<div class="col-xs-12 col-sm-4 col-md-4">
 		<form class="navbar-form navbar-right listSearchbox noBottomPadding" action="#" role="search">
 			<div class="form-group">
 				<div class="input-group">
@@ -26,8 +23,6 @@
 	</div>
 	<script type="text/javascript"> function studentSearch() {searchTiles('#studentScroller', changeCheck(), document.getElementById('navbarInput-01').value, "tileListudent");}</script>
 	<!-- End of search bit -->
-
-
 
 </div>
 <div class="row text-center">
