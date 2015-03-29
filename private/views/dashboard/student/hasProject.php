d40be6d6a12babf7610605023f0ccd6276a493b8
Matt Weeks <m_weeks@hotmail.co.uk>
2015-03-29 21:17:31 +0100

    Add separate dashboard if project has been submitted to CAS.

--- private/views/dashboard/student/hasProject.php
+++ private/views/dashboard/student/hasProject.php
@@ -12,13 +12,17 @@
 	loadQueue.push(function () {
 		document.querySelector(".chosenProject").innerHTML = document.querySelector(".chosenProject").innerHTML.replace("group", "<a href='/profile.php?type=group&id=" + me.group.id + "'>group</a>");
 		document.querySelector(".chosenProject").innerHTML = document.querySelector(".chosenProject").innerHTML.replace("project", "<a href='/profile.php?type=project&id=" + me.project.id + "'>project</a>");
-
-		if (me.group.creator.id == me.user.id) {
-			document.querySelector(".chosenProject").innerHTML += ("<p>Now you just need to submit the paperwork to the CAS office.</p>");
-			document.querySelector(".mainContent").innerHTML += ("<div class='jumbotron'><div class='container'><div class='row text-center'><section><p>Are you ready to submit your project application to the CAS office?</p></section><section><a class='btn btn-success' href='/intents.php?action=request&request=submitToCAS'>Let's do it!</a></section></div></div></div>");
+		if (me.project.hasCasSubmission) {
+			document.querySelector(".chosenProject").innerHTML += ("<p>And your paperwork has been submitted to the CAS office.</p>");
 		}
 		else {
-			document.querySelector(".chosenProject").innerHTML += ("<p>Now your group leader just needs to submit the paperwork to the CAS office.</p>");
+			if (me.group.creator.id == me.user.id) {
+				document.querySelector(".chosenProject").innerHTML += ("<p>Now you just need to submit the paperwork to the CAS office.</p>");
+				document.querySelector(".mainContent").innerHTML += ("<div class='jumbotron'><div class='container'><div class='row text-center'><section><p>Are you ready to submit your project application to the CAS office?</p></section><section><a class='btn btn-success' href='/intents.php?action=request&request=submitToCAS'>Let's do it!</a></section></div></div></div>");
+			}
+			else {
+				document.querySelector(".chosenProject").innerHTML += ("<p>Now your group leader just needs to submit the paperwork to the CAS office.</p>");
+			}
 		}
 	});
 </script>
