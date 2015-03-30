22129edf9f21d58c2852aad829b5e8fd85159e75
Matt Weeks <m_weeks@hotmail.co.uk>
2015-03-30 15:36:36 +0100

    List view button styling fix.

--- private/views/lists/groups.php
+++ private/views/lists/groups.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-xs-8 col-sm-8 col-md-8">
+	<div class="col-xs-12 col-sm-8 col-md-8">
 		<h1 class="reduceHeading hideEdit float-left listHeading">Groups</h1>
 		<div class="reduceTopMargin alignRight listButtonsDiv">
 			<div class="floatRight fui-new listButtons" onclick="alert();"></div>
