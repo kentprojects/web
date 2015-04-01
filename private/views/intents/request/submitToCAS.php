20322fcbffe1d9c6a1879c76b94d63785f8e21b4
Matt Weeks <m_weeks@hotmail.co.uk>
2015-04-01 17:00:01 +0100

    Add checkbox for special requirements on submission.

--- private/views/intents/request/submitToCAS.php
+++ private/views/intents/request/submitToCAS.php
@@ -1,6 +1,10 @@
 <h3 id="intentTitle">Submit to CAS office?</h3>
 
 <p id="intentDescription">Are you ready to submit your project to the CAS office?</p>
+<label class="checkbox" for="checkbox1">
+	<input type="checkbox" data-toggle="checkbox" checked id="checkbox1" class="custom-checkbox"><span class="icons"><span class="icon-unchecked"></span><span class="icon-checked"></span></span>
+	This project will entail research involving human participants as defined by the Faculty Research Ethics Procedures available <a href=“http://www.kent.ac.uk/stms/faculty/adminprocedures/research-ethics/index.html” target=“_blank”>here</a>.
+</label>
 
 <div class="btn-group">
 	<button class="btn btn-primary intentAccept" onclick="confirmRequest();" value="Confirm">
@@ -17,11 +21,28 @@
 
 	var loadQueue = loadQueue || [];
 	loadQueue.push(function () {
-
-		console.log(me.user);
-
 		confirmRequest = function confirmRequest() {
-			intentCreate("submit_to_cas", {groupId: me.group.id}, undefined);
+			API.POST(
+				'/intent',
+				{handler: "submit_to_cas", data: {additional: document.querySelector("#checkbox1").checked}},
+				cancelRequest,
+				function Error(data) {
+					if (data.status == 409) {
+						qf("#intentTitle", function (element) {
+							element.innerText = "You can't do that twice in a row!";
+						});
+						qf("#intentDescription", function (element) {
+							element.innerHTML =
+								'You need to wait until your current request is accepted or declined ' +
+								'before doing doing it again </br><strong><a href=# onclick="cancelRequest();"> ' +
+								'Go back? </a></strong>';
+						});
+
+					};
+					console.error(data);
+					document.querySelector(".btn-group").style.display = "none";
+				}
+			)
 		};
 		cancelRequest = function cancelRequest() {
 			window.location.href = "dashboard.php";
